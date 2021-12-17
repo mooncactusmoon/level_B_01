8dfe7f3a327cea527693a0f101ad569f18068b03
MoonLin <mooncactusmoon@gmail.com>
2021-12-17 15:37:15 +0800

    完成進站人數api縮成一行 ˋwˊ

--- api/total.php
+++ api/total.php
@@ -1,10 +1,12 @@
 <?php
 include_once "../base.php";
-
+/* 四行可縮成下面一行
 $views=$_POST['total'];
 $total=$Total->find(1);
 $total['total']=$views;
-$Total->save($total);
+$Total->save($total); */
+
+$Total->save(['id'=>1,'total'=>$_POST['total']]); 
 
 to("../back.php?do=total");
 
