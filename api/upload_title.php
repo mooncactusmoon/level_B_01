1dfd327832da3a4bf6c2efe568899074ab021432
MoonLin <mooncactusmoon@gmail.com>
2021-12-20 14:18:02 +0800

    完成網站標題圖片更新

---
+++ api/upload_title.php
@@ -0,0 +1,15 @@
+<?php
+include_once "../base.php";
+
+// dd($_POST);
+
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
+    $data['img']=$_FILES['img']['name'];
+    $data['id']=$_POST['id'];
+    $Title->save($data);
+}
+
+to("../back.php?do=".$Title->table);
+
+?>
